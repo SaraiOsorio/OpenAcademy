0b418d6a576b7b69c10e90c68acacd79eb22d0f9
SaraiOsorio <sarai_anai@hotmail.com>
2016-01-28 12:30:32 -0600

    [ADD] openacademy

---
+++ openacademy/__openerp__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+# © <YEAR(S)> <AUTHOR(S)>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Module name",
+    "summary": "Module summary",
+    "version": "9.0.1.0.0",
+    "category": "Uncategorized",
+    "website": "https://odoo-community.org/",
+    "author": "<AUTHOR(S)>, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "external_dependencies": {
+        "python": [],
+        "bin": [],
+    },
+    "depends": [
+        "base",
+    ],
+    "data": [
+        "view/some_model_view.xml",
+    ],
+    "demo": [
+        "demo/res_partner_demo.xml",
+    ],
+    "qweb": [
+        "static/src/xml/module_name.xml",
+    ]
+}
